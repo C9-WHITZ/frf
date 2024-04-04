de83f779e47e3dc257ad5ba817ab7e72cab59590
C9-WHITZ <142932816+C9-WHITZ@users.noreply.github.com>
2024-04-04 15:56:45 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function voltarTema(){
+    document.getElementById("fundo").style.background="black";
+    document.getElementById("fundo").style.color="white";
+}
+
+function mudarTema(){
+    document.getElementById("fundo").style.background="violet";
+    document.getElementById("fundo").style.color="black ";
+}
+
+function mouseForadaImagem(){
+    document.getElementById("imagem").src="123.jpg";
+}
+
+function mouseDentrodaImagem(){
+    document.getElementById("imagem").src="1234.jpg";
+}
